935fa94c69f749f5410721f12f95b940366cffb3
tianguangrong <17611505022@163.com>
2025-08-13 21:51:50 +0800

    弹框内容添加

--- src/views/chargingstation/comps/addMonitor.vue
+++ src/views/chargingstation/comps/addMonitor.vue
@@ -1,13 +1,30 @@
 
 <script setup lang="ts" name="AddMonitor">
-  import { ref } from 'vue';  
+  import { ref, reactive } from 'vue';
+  import type { FormInstance, FormRules } from 'element-plus';
+  interface RuleForm {
+    name: string,
+    fast: number | null
+  }
   const centerDialogVisible = ref(false);
   withDefaults(defineProps<{
     title?: string
   }>(), {
     title: '新增充电站'
   })
-  
+  const ruleForm = reactive<RuleForm>({
+    name: '',
+    fast: null,
+  });
+  const rules = reactive<FormRules<RuleForm>>({
+    name: [
+      { required: true, message: '充电站名称为必填项', trigger: 'blur' },
+    ],
+    fast: [
+      { required: true, message: '快充数为必填项', trigger: 'blur' },
+      { type: 'number', message: '快充数必须为数字类型' },
+    ],
+  })
   defineExpose({
     centerDialogVisible
   })
@@ -17,12 +34,26 @@
   <el-dialog
     v-model="centerDialogVisible"
     :title="title"
-    width="500"
+    draggable
+    width="70%"
     destroy-on-close
     right
   >
-    <div>
-      111
+    <div class="form-content">
+      <el-form
+        ref="monitorFormRef"
+        :model="ruleForm"
+        :rules="rules"
+        :inline="true"
+        label-width="auto"
+      >
+        <el-form-item label="充电站名称" prop="name" style="width: 45%;">
+          <el-input v-model="ruleForm.name" />
+        </el-form-item>
+        <el-form-item label="快充数" prop="fast"  style="width: 45%;">
+          <el-input v-model.number="ruleForm.fast" />
+        </el-form-item>
+      </el-form>
     </div>
     <template #footer>
       <div class="dialog-footer">
